9bfce612135f51ca74cdfe284a269721feb297fb
rsilveira1987 <rsilveira1987@live.com>
2017-04-20 01:57:15 -0300

    Add files via upload

---
+++ simplexhrfactory.js
@@ -0,0 +1,48 @@
+//
+// handle different browsers XHR object
+//
+
+var SimpleXhrFactory = (function(){
+
+	// private object instance
+	var instance;
+
+	// private mini-factory
+	var XMLHttpFactories = [
+ 		function() {return new XMLHttpRequest()},
+ 		function() {return new AcvtiveXObject("Msxml2.XMLHTTP")},
+ 		function() {return new AcvtiveXObject("Msxml3.XMLHTTP")},
+ 		function() {return new AcvtiveXObject("Microsoft.XMLHTTP")}
+	];
+
+	// private method
+	function createXMLHttpRequest() {
+
+		var xmlhttp = false;
+		
+		for (var i=0,num=XMLHttpFactories.length; i<num; i++) {
+			try {
+				xmlhttp = XMLHttpFactories[i]();
+			} catch (e) {
+				continue;
+			}
+			break;
+		}
+		
+		return xmlhttp;
+	}
+	
+	// public method
+	return {
+
+		getInstance: function(){
+			// control instance 
+			if (!instance) {
+				instance = createXMLHttpRequest();
+			}
+
+			return instance;
+		}
+	}
+
+})();
